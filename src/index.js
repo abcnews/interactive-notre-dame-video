74639dcfbdd2d8b188de6e714a490480319da411
Nathan Hoad <nathan@nathanhoad.net>
2019-04-18 06:18:20 +1000

    Removed the white box below the scrollyteller

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import App from './components/App';
 
 const scrollyteller = require('@abcnews/scrollyteller').loadOdysseyScrollyteller('video', 'u-full', 'mark');
 
+// Get rid of the white box below the scrollyteller
+if (document.location.href.indexOf('mobile') > -1) {
+  scrollyteller.mountNode.style.setProperty('margin-bottom', '-2rem');
+} else {
+  scrollyteller.mountNode.style.setProperty('margin-bottom', '-3rem');
+}
+
 function init() {
   render(<App scrollyteller={scrollyteller} />, scrollyteller.mountNode);
 }
